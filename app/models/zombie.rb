48644e46027ed4f17261955dfed532173af4e708
bassemawhoob <bassem.mawhoob@live.com>
2018-02-19 15:57:50 +0100

    Add Weapons and Armors to JSON Response

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -16,4 +16,8 @@ class Zombie < ApplicationRecord
   	scope :turn_date, -> (turn_date) {where turn_date: Date.parse(turn_date)}
   	scope :weapons, -> (weapons) {joins(:zombie_weapons).where("zombie_weapons.weapon_id IN (?)", weapons)}
   	scope :armors, -> (armors) {joins(:zombie_armors).where("zombie_armors.armor_id IN (?)", armors)}
+
+  	def as_json(*)
+        super(:except => [:created_at, :updated_at]).merge(weapons: weapons, armors:armors)
+    end
 end
